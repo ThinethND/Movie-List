3d9102862e88075e5b86ad74c98eb595e8532971
Diluksha Nimshan Munasinghe <167074999+ThinethND@users.noreply.github.com>
2025-06-19 18:16:04 +0530

    Created logic for search option.

--- movie-website-frontend/src/pages/Home.jsx
+++ movie-website-frontend/src/pages/Home.jsx
@@ -27,11 +27,23 @@ function Home() {
         loadPopularMovies()
     }, [])
 
-    const handleSearch = (e) => {
-        e.preventDefault()
-        alert(searchQuery)
-        setSearchQuery("")
-    }
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        if (!searchQuery.trim()) return  
+        if (loading) return
+
+        setLoading(true)
+        try{
+            const searchResults = await searchMovies(searchQuery)
+            setMovies(searchResults)
+            setError(null)
+        } catch (err) {
+            console.log(err)
+            setError("Failed to search movies..." )
+        } finally {
+            setLoading(false)
+        }
+    };
 
     return (
         <div className="home">
@@ -45,12 +57,17 @@ function Home() {
                     <button type="submit" className="search-button">Search</button>
                 </form>
 
-            <div className="movies-grid">
-                {movies.map((movie) => 
-                movie.title.toLowerCase().startsWith(searchQuery) && (
-                    <MovieCard movie={movie} key={movie.id} />
-                ))}
-            </div>
+                    {error && <div className="error-message">{error}</div>}
+                {loading ? (
+                    <div className="loading">Loading....</div>
+                ) : (
+                    <div className="movies-grid">
+                        {movies.map((movie) => 
+                        movie.title.toLowerCase().startsWith(searchQuery) && (
+                            <MovieCard movie={movie} key={movie.id} />
+                        ))}
+                    </div>
+                )}    
         </div>
     );
 }
