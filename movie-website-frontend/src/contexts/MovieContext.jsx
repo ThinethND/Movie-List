209c56f274ad30a4d29762c8ef9954651175eac6
Diluksha Nimshan Munasinghe <167074999+ThinethND@users.noreply.github.com>
2025-06-20 00:44:40 +0530

    Completed the website

--- movie-website-frontend/src/contexts/MovieContext.jsx
+++ movie-website-frontend/src/contexts/MovieContext.jsx
@@ -1,16 +1,16 @@
-import { createContext, useState, useContext, useEffect } from "react";
-import Favorites from "../pages/Favorites";
+import {createContext, useState, useContext, useEffect} from "react"
 
 const MovieContext = createContext()
 
 export const useMovieContext = () => useContext(MovieContext)
 
-export const MOvieProvider = ({chlidern}) => {
+export const MovieProvider = ({children}) => {
+    const [favorites, setFavorites] = useState([])
 
     useEffect(() => {
         const storedFavs = localStorage.getItem("favorites")
 
-        if (storedFavs) setFavorites(JSON.parse(storedFavs)) 
+        if (storedFavs) setFavorites(JSON.parse(storedFavs))
     }, [])
 
     useEffect(() => {
@@ -19,14 +19,14 @@ export const MOvieProvider = ({chlidern}) => {
 
     const addToFavorites = (movie) => {
         setFavorites(prev => [...prev, movie])
-    }  
-
-    const removeFromFavorites = (movieId) => {
-        return favorites.some(movie => movie.id === movieId)
     }
 
+    const removeFromFavorites = (movieId) => {
+        setFavorites(prev => prev.filter(movie => movie.id !== movieId))
+    }
+    
     const isFavorite = (movieId) => {
-        return favorites.some(movie => movieId.Id === movieId)
+        return favorites.some(movie => movie.id === movieId)
     }
 
     const value = {
@@ -36,8 +36,7 @@ export const MOvieProvider = ({chlidern}) => {
         isFavorite
     }
 
-    return <MovieContext.Provider>
-        {chlidern}
+    return <MovieContext.Provider value={value}>
+        {children}
     </MovieContext.Provider>
 }
-
